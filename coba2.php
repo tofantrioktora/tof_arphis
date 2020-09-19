e5e60431ac2ee86ed6e38de686a5f95051710602
TOFAN TRI OKTORA <arphis@gmail.com>
2020-09-19 08:41:13 +0700

    Add files via upload

---
+++ coba2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>coba2</title>
+</head>
+<body>
+<p>coba2</p>
+<form>
+<select name="users" onchange="showUser(this.value)">
+  <option value="">Select a person:</option>
+  <option value="1"><a href="coba1.php">coba1</a></option>
+  <option value="2"><a href="coba1.php">coba2</a></option>
+  <option value="3"><a href="coba1.php">coba3</a></option>
+  <option value="4">----</option>
+  </select>
+</form>
+<br>
+
+<a href="coba1.php">coba1</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+<p>home</p>
+<form>
+<select name="users" onchange="showUser(this.value)">
+  <option value="">Select a person:</option>
+  <option value="1">aaa</option>
+  <option value="2">bbb</option>
+  <option value="3">ccc</option>
+  <option value="4">----</option>
+  </select>
+</form>
+<br>
+
+<a href="coba1.php">coba1</a>
+</body>
+</html>
